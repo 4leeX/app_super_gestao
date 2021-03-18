b4108e5a623d145717e5ec903cbbe5749b634e4d
Alexsandro Ricardo <alexsandro.sousa12366@gmail.com>
2021-03-18 08:58:48 -0300

    autenticacao

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function index(Request $request){
+
+        $erro = '';
+
+        if($request->get('erro') == 1){
+            $erro = 'Usuário ou senha não existe!';
+        };
+
+        if($request->get('erro') == 2){
+            $erro = 'Necessário fazer login para ter acesso a página!';
+        };
+
+        return view('site.login', ['erro' => $erro]);
+    }
+
+    public function autenticar(Request $request){
+
+        //regras de validação
+        $regras = [
+            'usuario' => 'email',
+            'senha' => 'required'
+        ];
+
+        //as mensagens de feedback de validação
+        $feedback = [
+            'usuario.email' => 'O campo usuário (e-mail) é obrigatório',
+            'senha.required' => 'O campo senha é obrigatório'
+        ];
+
+        //se não passar pelo validate
+        $request->validate($regras, $feedback);
+
+        //recuperado parametros do formulário
+        $email = $request->get('usuario');
+        $password = $request->get('senha');
+
+        //iniciar o Model User
+        $user = new User();
+
+        $usuario = $user->where('email', $email)
+            ->where('password', $password)
+            ->get()
+            ->first();
+
+        if(isset($usuario->name)){
+
+            session_start();
+            $_SESSION['nome'] = $usuario->name;
+            $_SESSION['email'] = $usuario->email;
+
+            return redirect()->route('app.clientes');
+
+        }else{
+            return redirect()->route('site.login', ['erro' => 1 ]);
+        }
+    }
+}
